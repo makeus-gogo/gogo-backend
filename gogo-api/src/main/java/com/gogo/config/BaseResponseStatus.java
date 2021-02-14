84eac55b4928a83fccca5d888dbf2b85c0803be4
nays111 <43735448+nays111@users.noreply.github.com>
2021-02-14 15:42:14 +0900

    feat(jwt):jwt 토큰

---
+++ gogo-api/src/main/java/com/gogo/config/BaseResponseStatus.java
@@ -0,0 +1,41 @@
+package com.gogo.config;
+
+
+import lombok.Getter;
+
+/**
+ * 에러 코드 관리
+ */
+@Getter
+public enum BaseResponseStatus {
+    // 1000 : 요청 성공
+    SUCCESS(true, 1000, "요청에 성공하였습니다."),
+
+    // 2000 : Request 오류
+    REQUEST_ERROR(false, 2000, "입력값을 확인해주세요."),
+    EMPTY_USERID(false, 2001, "유저 아이디 값을 확인해주세요."),
+    EMPTY_JWT(false, 2010, "JWT를 입력해주세요."),
+    INVALID_JWT(false, 2011, "유효하지 않은 JWT입니다."),
+
+    // 3000 : Response 오류
+    RESPONSE_ERROR(false, 3000, "값을 불러오는데 실패하였습니다."),
+    NOT_FOUND_USER(false, 3010, "존재하지 않는 회원입니다."),
+
+    // 4000 : Database 오류
+    SERVER_ERROR(false, 4000, "서버와의 통신에 실패하였습니다."),
+    DATABASE_ERROR(false, 4001, "데이터베이스 연결에 실패하였습니다.");
+
+
+    // 5000 : 필요시 만들어서 쓰세요
+    // 6000 : 필요시 만들어서 쓰세요
+
+    private final boolean isSuccess;
+    private final int code;
+    private final String message;
+
+    private BaseResponseStatus(boolean isSuccess, int code, String message) {
+        this.isSuccess = isSuccess;
+        this.code = code;
+        this.message = message;
+    }
+}
