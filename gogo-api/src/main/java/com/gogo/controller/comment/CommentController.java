7dbdc9d93cd157ce83e03241241408f520a4182f
nays111 <43735448+nays111@users.noreply.github.com>
2021-02-25 12:41:58 +0900

    feat(comment) : 댓글 리스트 조회 API, 댓글 수정 API, 댓글 삭제 API

--- gogo-api/src/main/java/com/gogo/controller/comment/CommentController.java
+++ gogo-api/src/main/java/com/gogo/controller/comment/CommentController.java
@@ -4,11 +4,13 @@ import com.gogo.config.resolver.LoginUser;
 import com.gogo.controller.ApiResponse;
 import com.gogo.service.comment.CommentService;
 import com.gogo.service.comment.dto.request.CreateCommentRequest;
+import com.gogo.service.comment.dto.request.UpdateCommentRequest;
 import com.gogo.service.comment.dto.response.CommentInfoResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,5 +25,26 @@ public class CommentController {
                                                           @LoginUser Long memberId){
         return ApiResponse.of(commentService.createComment(createCommentRequest,uuid,memberId));
     }
+
+
+    @GetMapping("api/v1/board/{uuid}/comment")
+    public ApiResponse<List<CommentInfoResponse>> getCommentList(@PathVariable String uuid
+                                                                 ){
+        return ApiResponse.of(commentService.getCommentList(uuid));
+    }
+
+    @PatchMapping("api/v1/comment/{commentId}")
+    public ApiResponse<CommentInfoResponse> updateComment(@Valid @RequestBody UpdateCommentRequest updateCommentRequest,
+                                                          @LoginUser Long memberId,
+                                                          @PathVariable Long commentId){
+        return ApiResponse.of(commentService.updateComment(updateCommentRequest,commentId,memberId));
+    }
+
+    @PatchMapping("api/v1/comment/{commentId}/status")
+    public ApiResponse<String> deleteComment(@PathVariable Long commentId,@LoginUser Long memberId){
+        commentService.deleteComment(commentId,memberId);
+        return ApiResponse.OK;
+    }
+
 
 }
