db2ddea42b77e83c5836c2c084c5c559498e9127
nays111 <nays111@naver.com>
2021-02-25 12:44:10 +0900

    feat(comment) : 댓글 리스트 조회 API, 댓글 수정 API, 댓글 삭제 API (#10)

--- gogo-api/src/main/java/com/gogo/service/comment/CommentService.java
+++ gogo-api/src/main/java/com/gogo/service/comment/CommentService.java
@@ -1,22 +1,33 @@
 package com.gogo.service.comment;
 
 import com.gogo.domain.board.Board;
+import com.gogo.domain.board.BoardRepository;
 import com.gogo.domain.comment.Comment;
 import com.gogo.domain.comment.repository.CommentRepository;
 import com.gogo.service.board.BoardService;
 import com.gogo.service.board.dto.response.BoardDetailInfoResponse;
+import com.gogo.service.board.dto.response.BoardInfoResponse;
 import com.gogo.service.comment.dto.request.CreateCommentRequest;
+import com.gogo.service.comment.dto.request.UpdateCommentRequest;
 import com.gogo.service.comment.dto.response.CommentInfoResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
 
     private final CommentRepository commentRepository;
+    private final BoardRepository boardRepository;
 
     @Transactional
     public CommentInfoResponse createComment(CreateCommentRequest createCommentRequest, String uuid, Long memberId){
@@ -24,5 +35,46 @@ public class CommentService {
         Comment comment = new Comment(memberId,uuid,description);
         commentRepository.save(comment);
         return CommentInfoResponse.of(comment);
+    }
+
+    //todo 임시적인 페이징 -> 추후 수정 예정
+    @Transactional(readOnly = true)
+    public List<CommentInfoResponse> getCommentList(String uuid){
+        //Pageable pageable = PageRequest.of(page-1,size,Sort.by(Sort.Direction.ASC,"commentIdx"));
+        Board board = boardRepository.findBoardByUuid(uuid);
+        if (board == null) {
+            throw new IllegalArgumentException(String.format("해당하는 (%s)의 게시판은 존재하지 않습니다", uuid));
+        }
+        return commentRepository.findAllByUuidAndStatus(uuid,"ACTIVE").stream()
+            .map(CommentInfoResponse::of)
+            .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public CommentInfoResponse updateComment(UpdateCommentRequest updateCommentRequest, Long commentId,Long memberId){
+        String description = updateCommentRequest.getDescription();
+        Comment comment = commentRepository.findCommentByIdAndStatus(commentId,"ACTIVE");
+        if(comment==null){
+            throw new IllegalArgumentException(String.format("해당 댓글은 존재하지 않습니다."));
+        }
+        if(comment.getMemberId()!=memberId){
+            throw new IllegalArgumentException(String.format("본인이 쓴 댓글이 아닙니다."));
+        }
+        comment.setDescription(description);
+        commentRepository.save(comment);
+        return CommentInfoResponse.of(comment);
+    }
+
+    @Transactional
+    public void deleteComment(Long commentId,Long memberId){
+        Comment comment = commentRepository.findCommentByIdAndStatus(commentId,"ACTIVE");
+        if(comment==null){
+            throw new IllegalArgumentException(String.format("해당 댓글은 존재하지 않습니다."));
+        }
+        if(comment.getMemberId()!=memberId){
+            throw new IllegalArgumentException(String.format("본인이 쓴 댓글이 아닙니다."));
+        }
+        comment.setStatus("INACTIVE");
+        commentRepository.save(comment);
     }
 }
--- gogo-domain/src/main/java/com/gogo/domain/comment/repository/CommentRepository.java
+++ gogo-domain/src/main/java/com/gogo/domain/comment/repository/CommentRepository.java
@@ -1,9 +1,15 @@
 package com.gogo.domain.comment.repository;
 
 import com.gogo.domain.comment.Comment;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CommentRepository extends CrudRepository<Comment,Long> {
+    List<Comment> findAllByUuidAndStatus(String uuid, String status);
+    Comment findCommentByIdAndStatus(Long commentId,String status);
 }
