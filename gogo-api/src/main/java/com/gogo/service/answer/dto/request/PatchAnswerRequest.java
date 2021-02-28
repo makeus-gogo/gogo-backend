74ab99c377ee1fe0e5ccd4874e617fa52dbb5816
nays111 <43735448+nays111@users.noreply.github.com>
2021-02-28 22:44:08 +0900

    fix : 에러수정

--- gogo-api/src/main/java/com/gogo/service/answer/dto/request/PatchAnswerRequest.java
+++ gogo-api/src/main/java/com/gogo/service/answer/dto/request/PatchAnswerRequest.java
@@ -3,11 +3,9 @@ package com.gogo.service.answer.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
 @Getter
 @NoArgsConstructor
 public class PatchAnswerRequest {
-    //답변은 한개만 선택 가능
-    @NotBlank
+
     private Long contentId;
 }
