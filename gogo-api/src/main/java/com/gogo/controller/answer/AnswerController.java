12478fd610a463a470e59e7db9cb220bd624ebf0
nays111 <43735448+nays111@users.noreply.github.com>
2021-03-10 19:36:20 +0900

    feat : validation 추가 및 예외 처리

--- gogo-api/src/main/java/com/gogo/controller/answer/AnswerController.java
+++ gogo-api/src/main/java/com/gogo/controller/answer/AnswerController.java
@@ -2,6 +2,8 @@ package com.gogo.controller.answer;
 
 import com.gogo.config.resolver.LoginUser;
 import com.gogo.controller.ApiResponse;
+import com.gogo.exception.NotFoundException;
+import com.gogo.exception.ValidationException;
 import com.gogo.service.answer.AnswerService;
 import com.gogo.service.answer.dto.request.CreateAnswerRequest;
 import com.gogo.service.answer.dto.request.PatchAnswerRequest;
@@ -23,20 +25,33 @@ public class AnswerController {
 
     @PostMapping("api/v1/board/{boardId}/answer")
     @Operation(summary = "고민 답변 API",description = "boardId(고민게시글 인덱스), 토큰이 필요합니다, contentId(선택지 인덱스)")
+    @ResponseBody
     public ApiResponse<AnswerInfoResponse> createAnswer(@Valid @RequestBody CreateAnswerRequest createAnswerRequest,
                                                         @PathVariable Long boardId,
                                                         @LoginUser Long memberId) {
+        if(createAnswerRequest.getContentId()==null){
+            throw new ValidationException("고민에 대한 답변을 선택해주세요.","고민에 대한 답변을 선택해주세요.");
+        }
         return ApiResponse.of(answerService.createAnswer(createAnswerRequest,memberId,boardId));
     }
+
+
     @PatchMapping("api/v1/board/{boardId}/answer")
     @Operation(summary = "고민 답변 수정 API",description = "boardId(고민게시글 인덱스), 토큰이 필요합니다, contentId(선택지 인덱스)")
+    @ResponseBody
     public ApiResponse<AnswerInfoResponse> patchAnswer(@Valid @RequestBody PatchAnswerRequest patchAnswerRequest,
                                                         @PathVariable Long boardId,
                                                         @LoginUser Long memberId) {
+        if(patchAnswerRequest.getContentId()==null){
+            throw new ValidationException("고민에 대한 답변을 선택해주세요.","고민에 대한 답변을 선택해주세요.");
+        }
         return ApiResponse.of(answerService.patchAnswer(patchAnswerRequest,memberId,boardId));
     }
+
+
     @GetMapping("api/v1/board/{boardId}/answer")
     @Operation(summary = "고민 답변 결과 조회 API",description = "boardId(고민게시글 인덱스), 토큰이 필요합니다")
+    @ResponseBody
     public ApiResponse<AnswerResultResponse> getAnswer(@PathVariable Long boardId,
                                                        @LoginUser Long memberId){
         return new ApiResponse("","",answerService.getAnswer(boardId));
--- gogo-api/src/main/java/com/gogo/service/comment/CommentService.java
+++ gogo-api/src/main/java/com/gogo/service/comment/CommentService.java
@@ -4,9 +4,13 @@ import com.gogo.domain.board.Board;
 import com.gogo.domain.board.BoardRepository;
 import com.gogo.domain.comment.Comment;
 import com.gogo.domain.comment.repository.CommentRepository;
+import com.gogo.domain.member.Member;
+import com.gogo.domain.member.MemberRepository;
+import com.gogo.exception.NotFoundException;
 import com.gogo.service.comment.dto.request.CreateCommentRequest;
 import com.gogo.service.comment.dto.request.UpdateCommentRequest;
 import com.gogo.service.comment.dto.response.CommentInfoResponse;
+import com.gogo.service.comment.dto.response.CommentListInfoResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,25 +25,31 @@ public class CommentService {
 
     private final CommentRepository commentRepository;
     private final BoardRepository boardRepository;
+    private final MemberRepository memberRepository;
 
     @Transactional
     public CommentInfoResponse createComment(CreateCommentRequest createCommentRequest, Long boardId, Long memberId) {
         String description = createCommentRequest.getDescription();
-        Comment comment = new Comment(memberId, boardId, description);
+        Board board  = boardRepository.findBoardById(boardId);
+        if (board == null) {
+            throw new NotFoundException(String.format("해당하는 (%s)의 게시판은 존재하지 않습니다", boardId),"해당하는 게시물은 존재하지 않습니다.");
+        }
+        Member member = memberRepository.findMemberById(memberId);
+        Comment comment = new Comment(member, board, description);
         commentRepository.save(comment);
         return CommentInfoResponse.of(comment);
     }
 
     //todo 임시적인 페이징 -> 추후 수정 예정
     @Transactional(readOnly = true)
-    public List<CommentInfoResponse> getCommentList(Long boardId) {
+    public List<CommentListInfoResponse> getCommentList(Long boardId) {
         //Pageable pageable = PageRequest.of(page-1,size,Sort.by(Sort.Direction.ASC,"commentIdx"));
         Board board = boardRepository.findBoardById(boardId);
         if (board == null) {
-            throw new IllegalArgumentException(String.format("해당하는 (%s)의 게시판은 존재하지 않습니다", boardId));
+            throw new NotFoundException(String.format("해당하는 (%s)의 게시판은 존재하지 않습니다", boardId),"해당하는 게시물은 존재하지 않습니다.");
         }
         return commentRepository.findAllByBoardIdAndStatus(boardId, "ACTIVE").stream()
-            .map(CommentInfoResponse::of)
+            .map(CommentListInfoResponse::of)
             .collect(Collectors.toList());
     }
 
@@ -47,11 +57,12 @@ public class CommentService {
     public CommentInfoResponse updateComment(UpdateCommentRequest updateCommentRequest, Long commentId, Long memberId) {
         String description = updateCommentRequest.getDescription();
         Comment comment = commentRepository.findCommentByIdAndStatus(commentId, "ACTIVE");
+        Member member = memberRepository.findMemberById(memberId);
         if (comment == null) {
-            throw new IllegalArgumentException(String.format("해당 댓글은 존재하지 않습니다."));
+            throw new NotFoundException(String.format("해당 댓글은 존재하지 않습니다."),"해당 댓글은 존재하지 않습니다.");
         }
-        if (comment.getMemberId() != memberId) {
-            throw new IllegalArgumentException(String.format("본인이 쓴 댓글이 아닙니다."));
+        if (comment.getMember().getId() != member.getId()) {
+            throw new NotFoundException(String.format("본인이 쓴 댓글이 아닙니다."),"본인이 쓴 댓글이 아닙니다.");
         }
         comment.setDescription(description);
         commentRepository.save(comment);
@@ -62,10 +73,11 @@ public class CommentService {
     public void deleteComment(Long commentId, Long memberId) {
         Comment comment = commentRepository.findCommentByIdAndStatus(commentId, "ACTIVE");
         if (comment == null) {
-            throw new IllegalArgumentException(String.format("해당 댓글은 존재하지 않습니다."));
+            throw new NotFoundException(String.format("해당 댓글은 존재하지 않습니다."),"해당 댓글은 존재하지 않습니다.");
         }
-        if (comment.getMemberId() != memberId) {
-            throw new IllegalArgumentException(String.format("본인이 쓴 댓글이 아닙니다."));
+        Member member = memberRepository.findMemberById(memberId);
+        if (comment.getMember().getId() != member.getId()) {
+            throw new NotFoundException(String.format("본인이 쓴 댓글이 아닙니다."),"본인이 쓴 댓글이 아닙니다.");
         }
         comment.setStatus("INACTIVE");
         commentRepository.save(comment);
