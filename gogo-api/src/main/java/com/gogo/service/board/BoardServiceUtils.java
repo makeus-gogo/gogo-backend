59416dbf25cb0b918a125183441b5b9f142e46fc
Seungho Kang <will.seungho@gmail.com>
2021-03-23 21:42:00 +0900

    fix: 키워드를 입력하지 않으면, 전체 조회가 되도록 수 (#21)

--- gogo-api/src/main/java/com/gogo/service/board/BoardServiceUtils.java
+++ gogo-api/src/main/java/com/gogo/service/board/BoardServiceUtils.java
@@ -6,6 +6,8 @@ import com.gogo.exception.NotFoundException;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 class BoardServiceUtils {
 
@@ -15,6 +17,20 @@ class BoardServiceUtils {
             throw new NotFoundException(String.format("해당하는 (%s)의 게시판은 존재하지 않습니다", boardId), "해당하는 게시물은 존재하지 않습니다.");
         }
         return board;
+    }
+
+    public static List<Board> getBoardsByLikeKeyword(BoardRepository boardRepository, String keyword, Long lastBoardId, int size) {
+        if (lastBoardId == 0) {
+            return boardRepository.findLastBoardsByLikeTitle(keyword, size);
+        }
+        return boardRepository.findBoardsByLikeTitle(keyword, lastBoardId, size);
+    }
+
+    public static List<Board> findBoardsOrderByIdWithPagination(BoardRepository boardRepository, Long lastBoardId, int size) {
+        if (lastBoardId == 0) {
+            return boardRepository.findBoardsOrderByIdDesc(size);
+        }
+        return boardRepository.findBoardsLessThanOrderByIdDescLimit(lastBoardId, size);
     }
 
 }
