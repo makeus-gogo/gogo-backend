7fa2f7a0955232a25de006a8c600c455ef0ce4d8
nays111 <nays111@naver.com>
2021-03-25 03:39:09 +0900

    fix : 고민 상세조회시 답변 결과 함께 반환 (#22)
    * feat : TOP3 고민글 조회 API
    
    * fix : 고민 상세 조회할 때 답변 결과도 함께 반환

--- gogo-api/src/main/java/com/gogo/service/answer/dto/response/AnswerResultResponse.java
+++ gogo-api/src/main/java/com/gogo/service/answer/dto/response/AnswerResultResponse.java
@@ -1,7 +1,10 @@
 package com.gogo.service.answer.dto.response;
 
+import com.gogo.domain.board.BoardType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -9,6 +12,11 @@ import java.util.List;
 public class AnswerResultResponse {
     private final Long boardId;
     private final String description;
+    private final String pictureUrl;
+    private final BoardType type;
+    private final LocalDateTime startDateTime;
+    private final LocalDateTime endDateTime;
+    private final List<String> hashTags;
     private final List<AnswerResultDto> answerResultDtoList;
 
 }
